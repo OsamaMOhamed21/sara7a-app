a004d40f3fc4bd75b562ae5236ed71ac537d8593
YassinAbdelWahed <yassinmohmed67@gmail.com>
2026-09-22 21:35:25 +0300

    user

--- src/app.controller.js
+++ src/app.controller.js
@@ -12,7 +12,6 @@ const bootstrap = async () => {
   app.use(express.json());
   // app routing
   app.use("/auth", authController);
-  app.use("/user", userController);
   app.get("/", (req, res, next) => res.json({ message: "Done" }));
   app.get("{/*dummy}", (req, res, next) =>
     res.status(404).json({ message: "In-Valid Routing" }),
